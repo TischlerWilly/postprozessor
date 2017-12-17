147486a2f7c76f9b079fac588ea71c8962ceeb3e
TischlerWilly <oliver.schuft@gmx.de>
2017-12-17 14:57:25 +0100

    import HBEx+ und HBEx-

--- Definitionen/def_bearbeitungen.h
+++ Definitionen/def_bearbeitungen.h
@@ -2,12 +2,13 @@
 #define DEF_BEARBEITUNGEN_H
 
 
-#define WST_BEZUG_OBSEI     "bez_obsei"     //Bezugsfläche Wst-Oberseite
-#define WST_BEZUG_UNSEI     "bez_unsei"     //Bezugsfläche Wst-Unterseite
-#define WST_BEZUG_LI        "bez_li"        //Bezugsfläche Wst-Schmalfläche links
-#define WST_BEZUG_RE        "bez_re"        //Bezugsfläche Wst-Schmalfläche rechts
-#define WST_BEZUG_OB        "bez_ob"        //Bezugsfläche Wst-Schmalfläche oben
-#define WST_BEZUG_UN        "bez_un"        //Bezugsfläche Wst-Schmalfläche unten
+#define WST_BEZUG_OBSEI     "bezug_obsei"     //Bezugsfläche Wst-Oberseite
+#define WST_BEZUG_UNSEI     "bezug_unsei"     //Bezugsfläche Wst-Unterseite
+//der Programminterne WST-Nullpunkt ist links-unten
+#define WST_BEZUG_LI        "bezug_li"        //Bezugsfläche Wst-Schmalfläche links (Westen)
+#define WST_BEZUG_RE        "bezug_re"        //Bezugsfläche Wst-Schmalfläche rechts (Osten)
+#define WST_BEZUG_VO        "bezug_vo"        //Bezugsfläche Wst-Schmalfläche vorne (Süden)
+#define WST_BEZUG_HI        "bezug_hi"        //Bezugsfläche Wst-Schmalfläche hinten (Norden)
 
 #define TRENNZ_BEARB_PARAM  '\t'             //Trennzeichen zur Trennung der einzelnen Parameter einer Bearbeitung
 #define TRENNZ_BEARB_PARAM_ "\t"
@@ -21,7 +22,8 @@
     4: Tiefe
     5: Posision in X
     6: Position in Y
-    7: Ausführbedingung
+    7: Position in Z
+    8: Ausführbedingung
 */
 
 
